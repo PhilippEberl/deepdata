d848b68d059d7a8cbb245657780096549a6093bf
Philipp Eberl <peberl.its-b2018@fh-salzburg.ac.at>
2021-04-20 15:50:19 +0200

    first changes with mars data

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+export class MarsData {
+  temperature: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  values: JSON;
+  sol: number;
+
+  constructor(private http: HttpClient){}
+
+
+  public getValues() {
+    this.http.get<JSON>('https://api.nasa.gov/insight_weather/?api_key=PDUHpwFVlCIf6P0a6CbEQZJTvg58mSFhv2g2LYEL&feedtype=json&ver=1.0')
+      .subscribe((data) => {
+        this.parseObject(data);
+        this.values = data;
+        //console.log(data);
+      });
+  }
+
+  public parseObject(obj)
+{
+   for(let key in obj)
+   {
+      console.log("key: " + key + ", value: " + obj[key])
+      if(obj[key] instanceof Object)
+      {
+        this.parseObject(obj[key]);
+      }
+   }
+}
 }
--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
